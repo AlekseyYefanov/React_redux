850f8ec8977b15f0039dced40502b392438d2235
Yefanov <yefanovaleksey@gmail.com>
2020-01-07 14:43:01 +0200

    create nav sidebar and profile components

---
+++ social_network/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://www.8host.com/blog/wp-content/themes/wp-8host-new/images/logo-8host.png' />
+            </header>
+        </div>
+    );
+};
+
+export default Header;
---
+++ social_network/src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://prm.ua/wp-content/uploads/2019/08/1481646441_1.jpg'/>
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                My posts
+                <div>Item post</div>
+                <div>Item post</div>
+                <div>Item post</div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Profile;
